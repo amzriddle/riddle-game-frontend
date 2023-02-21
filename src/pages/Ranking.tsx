44f1dea7f10b08b0df7c5642864e9f67eeaee08a
darth-ahsoka <79472350+amzriddle@users.noreply.github.com>
2023-02-21 05:58:44 +0000

    fix: ranking full page

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -52,55 +52,55 @@ function Ranking() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 12,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Grid>
-          <CssBaseline />
-          <Grid item xs={12}>
-            <Typography variant="h3">Ranking</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TableContainer component={Paper}>
-              <Table aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Rank</TableCell>
-                    <TableCell align="right">email</TableCell>
-                    <TableCell align="right">Complete</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {ranking.map((row) => (
-                    <TableRow
-                      key={row.email}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.rank}
-                      </TableCell>
-                      <TableCell align="right">{row.email}</TableCell>
-                      <TableCell align="right">
-                        {row.completeChallengesCount}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-          <Grid item xs={12}>
-            <Pagination count={10} page={page} onChange={handleChange} />
-          </Grid>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        p: 3,
+        marginTop: 12,
+      }}
+    >
+      <Grid>
+        <CssBaseline />
+        <Grid item xs={12}>
+          <Typography variant="h3">Ranking</Typography>
         </Grid>
-      </Box>
-    </Container>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Rank</TableCell>
+                  <TableCell align="right">email</TableCell>
+                  <TableCell align="right">Complete</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ranking.map((row) => (
+                  <TableRow
+                    key={row.email}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.rank}
+                    </TableCell>
+                    <TableCell align="right">{row.email}</TableCell>
+                    <TableCell align="right">
+                      {row.completeChallengesCount}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ alignItems: "center" }}>
+            <Pagination count={10} page={page} onChange={handleChange} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
